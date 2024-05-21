3152d49c4abd9882d2b05a62cb8d223330d8015a
arianesouza <arianesouzareal@gmail.com>
2024-05-21 06:09:03 -0300

    sec 24- Rendered the app titles into columns

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas
+
 
 st.set_page_config(layout='wide')
 col1, col2 = st.columns(2)
@@ -8,11 +10,29 @@ with col1:
 
 with col2:
     st.title('Ariane Souza')
-    content = """ Hi there, this is Alex:)🌺 I am a passionate graphic and brand designer. 
-    I specialize in creating colorful and bold brands that truly stand out. From vibrant logos to eye-catching advertisements, 
-    I love to play with colors and push the boundaries of traditional design. ✨Let's work together"""
+    content = """ Hi there, this is Ariane :)🚀 I am a passionate Python and Full Stack Developer. 
+    I specialize in bringing projects to life through coding and solving complex UX/UI problems.
+    From building robust web applications to enhancing user experiences, I love crafting elegant
+    solutions that push the boundaries of technology and design. 
+    🌟 Let's work together to turn your ideas into reality!"""
     st.write(content)
 
-with st.container():
-    st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
+content2 = """ Below you can find some of the apps I have built in Python. Feel free to contact me!"""
+
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+
+
+
+
 

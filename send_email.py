7db96cf560c03b98ef59c3f85c696ef45a08e1f2
arianesouza <arianesouzareal@gmail.com>
2024-05-27 08:07:26 -0300

    Sec 25 - Sent emails using the web form

--- send_email.py
+++ send_email.py
@@ -1,20 +1,26 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "hellokinkoa@gmail.com"
+    password = "hhfghgdnxlcjyjkp"  # It's not secure to hardcode passwords in your code
+    receiver = "hellokinkoa@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
 
-username = "hellokinkoa@gmail.com"
-password = "hhfghgdnxlcjyjkp"
-
-receiver = "hellokinkoa@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
+# Define the email message
+email_message = """\
 Subject: Hi!
+
 How are you?
 Bye!
 """
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username,receiver, message )
+
+# Call the function with the email message
+send_email(email_message)
